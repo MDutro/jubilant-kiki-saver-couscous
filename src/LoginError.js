b7a8a4193cb6942a4204695e7cd5824938a07224
Micah Dutro <51960903+MDutro@users.noreply.github.com>
2020-04-02 18:01:45 -0500

    Update src/LoginError.js
    Co-Authored-By: Eliot Sanford <eliot587@gmail.com>

--- src/LoginError.js
+++ src/LoginError.js
@@ -4,7 +4,7 @@ function LoginError({onLogin}){
     return(
       <main>
         <h1>Login Unsuccessful!</h1>
-        <p><span id='username'>Hey Student!</span> there was an error with your attendance! Please try again!</p>
+        <p>Hey <span id='username'>Student</span>! There was an error with your attendance. Please try again!</p>
         <button onClick={ () => onLogin('login')}>
             Login Again
         </button>
