30d2cb496f220b6106b8bd48f0e0b8e890ca0301
Garrick Crouch <services@localpathcomputing.com>
2020-04-14 19:38:46 -0500

    add useState w/hooks, remove classes

--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -1,41 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+function ResetPassword() {
 
-export default class ResetPassword extends React.Component {
+  const [email, setEmail] = useState('')
 
-  constructor(props) {
-    super(props)  
-    this.state = {email: null}
-  }
-
-  handleChange = (e) => {
-    let email = e.target.value;
-    this.setState({ email: email
-    })
-  }
-
-  handleSubmit = async () => {
+  async function handleSubmit() {
     
     const formData = new FormData();
-        formData.append('email', this.state.email);
-        console.log(this.state);
+    formData.append('email', email);
 
-        const response = await fetch('http://localhost:3000/reset', {
-            method: 'POST',
-            body: formData
-        });
-        console.log(response);
+    const response = await fetch('http://localhost:3000/reset', {
+      method: 'POST',
+      body: formData
+    });
+    console.log(response);
   }
 
-  render() {
-    return(
-      <div id="container">
-        <input onChange={this.handleChange} id="email" name="email" type="email" placeholder="email address"/>
-        <button onClick={this.handleSubmit} className="btn">Reset Password</button>
-      </div>
-    )
-  }
-  
-
+  return (
+    <div id="container">
+      <input onChange={e => setEmail(e.target.value)  } id="email" name="email" type="email" placeholder="email address" />
+      <button onClick={handleSubmit} className="btn">Reset Password</button>
+    </div>
+  );
 
 }
+
+export default ResetPassword;
